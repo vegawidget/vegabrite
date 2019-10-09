4939e55f85033d1b456e9d76adecc4f52b97f14b
Alicia Schep <aschep@gmail.com>
2019-10-08 20:30:46 -0700

    fix facet wrap issue

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -8,3 +8,13 @@ test_that("can add properties to a chart", {
   expect_equivalent(chart$height, 300)
   expect_equivalent(chart$width, 50)
 })
+
+
+test_that("can use vl_facet_wrap with columns argument", {
+  
+  chart <- vl_chart() %>%
+    vl_facet_wrap(field = "x", type = "quantitative", columns = 2) 
+  
+  expect_equivalent(chart$columns, 2)
+  expect_equivalent(chart$facet, list(field = "x", type = "quantitative"))
+})
