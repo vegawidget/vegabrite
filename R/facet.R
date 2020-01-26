03e9c4abd0a0fc95d5f1dbdf8112311469285d05
Alicia Schep <aschep@gmail.com>
2020-01-26 12:54:57 -0800

    update facet

--- R/facet.R
+++ R/facet.R
@@ -5,7 +5,7 @@
 
 
 .add_facet.vegaspec_facet <- function(spec, obj, ref, .type, columns = NULL) {
-  if (.type == "facet" && is.null(columns)) {
+  if (.type == "wrap" && is.null(columns)) {
     columns <- obj$columns
     obj$columns <- NULL
   }
@@ -22,6 +22,32 @@
   return(vegawidget::as_vegaspec(spec))
 }
 
+.add_facet.vegaspec_layer <- function(spec, obj, ref, .type, columns = NULL) {
+  keep <- c("$schema", "data")
+  keep <- keep[which(keep %in% names(spec))]
+  move <- names(spec)[which(!(names(spec) %in% keep))]
+  old_spec <- spec
+  spec <- spec[keep]
+  spec$spec <- old_spec[move]
+  
+  if (.type == "wrap" && is.null(columns)) {
+    columns <- obj$columns
+    obj$columns <- NULL
+  }
+  
+  validate_sub_schema(obj, ref)
+  if (.type == "row") {
+    spec[["facet"]][["row"]] <- obj
+  } else if (.type == "column") {
+    spec[["facet"]][["column"]] <- obj
+  } else {
+    spec[["facet"]] <- obj
+    spec[["columns"]] <- columns
+  }
+  
+  return(vegawidget::as_vegaspec(spec))
+}
+
 .add_facet.vegaspec_unit <- function(spec, obj, ref, .type, columns = NULL) {
   keep <- c("$schema", "data")
   keep <- keep[which(keep %in% names(spec))]
@@ -30,7 +56,7 @@
   spec <- spec[keep]
   spec$spec <- old_spec[move]
 
-  if (.type == "facet" && is.null(columns)) {
+  if (.type == "wrap" && is.null(columns)) {
     columns <- obj$columns
     obj$columns <- NULL
   }
