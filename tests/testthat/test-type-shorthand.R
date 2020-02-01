afee3a78c76739e6daa9482d48c2c8f3bfb43ca3
Alicia Schep <aschep@gmail.com>
2020-01-31 18:34:40 -0800

    tooltip and facet sugar

--- tests/testthat/test-type-shorthand.R
+++ tests/testthat/test-type-shorthand.R
@@ -38,3 +38,42 @@ test_that("can use shorthand type with .object input", {
   expect_equivalent(chart$encoding$x$field, "abba")
   expect_equivalent(chart$encoding$x$type, "ordinal")
 })
+
+test_that("can use shorthand type with a list of inputs", {
+  chart <- vl_chart() %>%
+    vl_encode_tooltip(c("abba:O","baba:N"))
+  
+  expect_equivalent(chart$encoding$tooltip[[1]]$field, "abba")
+  expect_equivalent(chart$encoding$tooltip[[1]]$type, "ordinal")
+  expect_equivalent(chart$encoding$tooltip[[2]]$field, "baba")
+  expect_equivalent(chart$encoding$tooltip[[2]]$type, "nominal")
+})
+
+test_that("can use shorthand type with a list of lists of inputs", {
+  chart <- vl_chart() %>%
+    vl_encode_tooltip(list(list(field = "abba:O"),list(field = "baba:N")))
+  
+  expect_equivalent(chart$encoding$tooltip[[1]]$field, "abba")
+  expect_equivalent(chart$encoding$tooltip[[1]]$type, "ordinal")
+  expect_equivalent(chart$encoding$tooltip[[2]]$field, "baba")
+  expect_equivalent(chart$encoding$tooltip[[2]]$type, "nominal")
+})
+
+test_that("can use shorthand for facets", {
+  chart <- vl_chart() %>%
+    vl_facet_row(field = "abba:O")
+  
+  expect_equivalent(chart$facet$row$field, "abba")
+  expect_equivalent(chart$facet$row$type, "ordinal")
+})
+
+
+test_that("can use shorthand for facets as first arg", {
+  chart <- vl_chart() %>%
+    vl_facet_row("abba:O")
+  
+  expect_equivalent(chart$facet$row$field, "abba")
+  expect_equivalent(chart$facet$row$type, "ordinal")
+})
+
+
