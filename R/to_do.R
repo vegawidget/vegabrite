69e833ac093aa083925c3b7977a599768ab22dca
Alicia Schep <aschep@gmail.com>
2021-11-07 23:02:17 -0800

    add todos

---
+++ R/to_do.R
@@ -0,0 +1,7 @@
+# To Do:
+# Fix up other examples
+# Improve way of passing selection config -- is this vl$PointSelectionConfig and documentation about how to use?
+# More objects to add?
+# consider collapsing add_selection_parameter and add_variable_parameter
+# consider splitting add_selection_parameter to add_point_selection_parameter
+# and add_interval_selection_parameter and flattening in the configs?
