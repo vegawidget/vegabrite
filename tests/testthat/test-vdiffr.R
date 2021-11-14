5031ee41a3416f2089859a71aa97e914fefb9910
Alicia Schep <aschep@gmail.com>
2021-11-14 09:36:31 -0800

    add test

--- tests/testthat/test-vdiffr.R
+++ tests/testthat/test-vdiffr.R
@@ -15,3 +15,20 @@ vdiffr::expect_doppelganger(
   simple_scatterplot, 
   writer = test_writer
 )
+
+repeat_scatterplot <- vl_chart() %>%
+  vl_add_data(values = mtcars) %>%
+  vl_mark_point() %>%
+  vl_encode_x(field = "hp", type = "quantitative") %>%
+  vl_encode_y(field = "mpg", type = "quantitative") %>%
+  vl_encode_color(field = list("repeat" = "column"), type = "nominal") %>%
+  vl_repeat_col("cyl", "gear") %>%
+  vl_resolve_scale_color("independent")
+
+vdiffr::expect_doppelganger(
+  "Repeat scatterplot", 
+  repeat_scatterplot, 
+  writer = test_writer
+)
+
+
